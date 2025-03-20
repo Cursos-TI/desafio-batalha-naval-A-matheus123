4f74dfad1a220d67da7cf2495339c72a32a45e8d
A-matheus123 <arteprivada.220watts@gmail.com>
2025-03-20 16:04:36 +0000

    nível mestre concluído!!

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 
+#define LINHA 10
+#define COLUNA 10
+
 int main() {
+
+
     // matriz do tabuleiro 10 por 10:
-    char tabuleiro[10][10];
+    char tabuleiro[LINHA][COLUNA];
 
     // lógica do tabuleiro com '0' representando a água:
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
+    for (int i = 0; i < LINHA; i++) {
+        for (int j = 0; j < COLUNA; j++) {
             tabuleiro[i][j] = '0';
         }
     }
@@ -14,7 +19,7 @@ int main() {
     // posicionando o primeiro navio na horizontal:
     int linhaH = 2;  
     int colunaH = 2; 
-    for (int j = 1; j < 4; j++) {
+    for (int j = 0; j < 3; j++) {
         tabuleiro[linhaH][colunaH + j] = '3';
     }
 
@@ -26,8 +31,8 @@ int main() {
     }
 
     // posicionando o terceiro navio na diagonal (três posições a partir de tabuleiro[5][4]):
-    int colunaD = 4;
-    int linhaD = 5;
+    int colunaD = 3;
+    int linhaD = 6;
     for (int i = 0; i < 3; i++) {
         tabuleiro[linhaD + i][colunaD - i] = '3';
     }
@@ -39,11 +44,44 @@ int main() {
         tabuleiro[linhaD2 - i][colunaD2 + i] = '3';
     }
 
+    // posicionando habilidade em cone:
+    int colunaHabCo = 5;
+    int linhaHabCo = 7;
+    for (int i = 0; i < 3; i++) {
+        tabuleiro[linhaHabCo + i][colunaHabCo - i] = '5';
+        tabuleiro[linhaHabCo + i][colunaHabCo + i] = '5';
+        tabuleiro[linhaHabCo + i][colunaHabCo] = '5';
+        tabuleiro[9][4] = '5';
+        tabuleiro[9][6] = '5';
+    }
+
+    // posicionando a habilidade em cruz:
+    int colunaHabCr = 6;
+    int linhaHabCr = 3;
+    for (int i = 0; i < 3; i++) {
+        tabuleiro[linhaHabCr][colunaHabCr + i] = '5';
+        tabuleiro[linhaHabCr + i][colunaHabCr] = '5';
+        tabuleiro[linhaHabCr][colunaHabCr - i] = '5';
+        tabuleiro[linhaHabCr - i][colunaHabCr] = '5';
+    }
+
+    // posicionando a habilidade em octaedro:
+    int colunaHabOct = 1;
+    int linhaHabOct = 5;
+    for (int i = 0; i < 2; i++) {
+        tabuleiro[linhaHabOct + i][colunaHabOct] = '5';
+        tabuleiro[linhaHabOct][colunaHabOct + i] = '5';
+        tabuleiro[linhaHabOct - i][colunaHabOct] = '5';
+        tabuleiro[linhaHabOct][colunaHabOct - i] = '5';
+
+    }
+
+
     // impressão do tabuleiro:
     printf("   A B C D E F G H I J\n");
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < LINHA; i++) {
         printf("%2d ", i + 1);
-        for (int j = 0; j < 10; j++) {
+        for (int j = 0; j < COLUNA; j++) {
             printf("%c ", tabuleiro[i][j]);
         }
         printf("\n");
